81d253d428cfd04a1d40f93cb6c7a93706ed1c34
NaborUsa <39609083+NaborUsa@users.noreply.github.com>
2023-12-19 12:20:31 -0500

    Add files via upload

---
+++ dwm/patch/include.h
@@ -0,0 +1,24 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "autostart.h"
+#include "cyclelayouts.h"
+#include "renamed_scratchpads.h"
+#include "sticky.h"
+/* Layouts */
+#include "layout_columns.h"
+#include "layout_deck.h"
+#include "layout_fibonacci.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
